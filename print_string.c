7854d78cfabc307b6bf741491b6b8f7ea47c441e
fourbet <ofourbet@yahoo.fr>
2020-10-25 16:38:00 +0000

    add NULL case

--- print_string.c
+++ print_string.c
@@ -1,4 +1,21 @@
 #include "holberton.h"
+/**
+ * print_str - prints the string (null)
+ *
+ * Return: length of (null)
+ */
+int print_str(void)
+{
+	int i = 0;
+	char *string = "(null)";
+
+	while (string[i] != '\0')
+	{
+		_putchar(string[i]);
+		i++;
+	}
+	return (6);
+}
 
 /**
  * print_string - print a string given
@@ -9,8 +26,14 @@ int print_string(va_list arg)
 {
 	char *ptr = va_arg(arg, char *);
 	int i, length_arg = 0;
+	int j = 0;
 
-	if (ptr != 0)
+	if (ptr == NULL)
+	{
+		j = print_str();
+		return (j);
+	}
+	else
 	{
 		length_arg = _strlen(ptr);
 		for (i = 0; ptr[i] != '\0'; i++)
