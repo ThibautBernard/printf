3083de148da6f9f006e506c9e4971515d23df153
Thibaut <ThibautHolberton>
2020-10-25 21:08:59 +0000

    fix(): create another function char for the case if null and free malloc

--- print_reverse.c
+++ print_reverse.c
@@ -1,22 +1,21 @@
 #include "holberton.h"
-/**
- * print_reverse - print string reverse
- * @a: argument
- * Return: number of char printed
- */
-int print_reverse(va_list a)
-{
-	int y, i, length = 0;
-	char *s, *str_fill;
 
-	s = va_arg(a, char *);
-	if (s == NULL)
-		return (print_null());
-	length = _strlen(s);
+
+/**
+ * rev - malloc an array and fill
+ * @s: the string
+ * @length: length of the string
+ * Return: string filled
+ */
+char *rev(char *s, int length)
+{
+	int i, y;
+	char *str_fill;
+
 	str_fill = malloc(sizeof(char) * (length + 1));
 	if (str_fill == NULL)
-		exit(98);
-	i = length;
+		return (NULL);
+	i = length - 1;
 	y = 0;
 	for (; i >= 0; i--)
 	{
@@ -24,8 +23,26 @@ int print_reverse(va_list a)
 		y++;
 	}
 	str_fill[y] = '\0';
-	for (i = 0; i <= length; i++)
-		_putchar(str_fill[i]);
-	free(str_fill);
-	return ((i - 1));
+	return (str_fill);
+}
+
+/**
+ * print_reverse - print string reverse
+ * @a: argument
+ * Return: number of char printed
+ */
+int print_reverse(va_list a)
+{
+	int  i, length = 0;
+	char *s, *str_filled;
+
+	s = va_arg(a, char *);
+	if (s == NULL)
+		return (print_null());
+	length = _strlen(s);
+	str_filled = rev(s, length);
+	for (i = 0; i < length; i++)
+		_putchar(str_filled[i]);
+	free(str_filled);
+	return ((i));
 }
