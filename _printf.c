25dc53a456fc4dd05136d23f8f54c91995c6e8a9
Thibaut <ThibautHolberton>
2020-10-25 09:28:38 +0000

    refactor && modify the logic

--- _printf.c
+++ _printf.c
@@ -1,56 +1,49 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
  * _print_count - print char and incremente the counter
- *
- * @i: int
+ * @format: the string to print and count
  * @counter: int
- *
- * Return: int i
+ * Return: length of the string printed
  */
 int _print_count(char format, int counter)
 {
 	counter++;
 	_putchar(format);
-	return(counter);
+	return (counter);
 }
 
 /**
  * _printf - main function printf according the format
  * @format: the string given
- * Return: the length:
+ * Return: the length of all the characters printed
  */
 int _printf(const char *format, ...)
 {
 	int i, y, counter = 0, sum = 0;
-	filt arr[] = {{"c", print_char},{"s", print_string},{NULL, NULL}};
+	filt arr[] = {{"c", print_char}, {"s", print_string}, {"d", print_decimal},
+{"i", print_decimal}, {NULL, NULL}};
 	va_list args;
 
 	va_start(args, format);
 	for (i = 0; format[i] != '\0'; i++)
 	{
-		if (format[i] == '%' && format[i + 1] != '\0')
+		if (format[i] == '%')
 		{
 			i++;
 			for (y = 0; arr[y].identifier != NULL; y++)
 			{
-					if (format[i] == *arr[y].identifier)
-					{
-						sum = arr[y].ptr_f(args);
-						counter += sum;
-						if (format[i + 1] != '\0')
-						{	
-							i++;
-							if (format[i] != '%')
-								counter = _print_count(format[i], counter);
-						}
-					}
+				if (format[i] == *arr[y].identifier)
+				{
+					sum = arr[y].ptr_f(args);
+					counter += sum;
+				}
 			}
-			if (format[i] == '%')
+			if (format[i] == '%' && format[i - 1] == '%')
 				counter = _print_count(format[i], counter);
 		}
-		else if (format[i] != '%' || format[i + 1] != '\0')
+		else
 			counter = _print_count(format[i], counter);
 	}
+	va_end(args);
 	return (counter);
 }
