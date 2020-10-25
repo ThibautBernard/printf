7a19e63b3ac3f2a50a9c8d44dd61250cccdec0c7
Thibaut <ThibautHolberton>
2020-10-25 09:46:18 +0000

    feat(print_decimal)

---
+++ print_decimal.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+int print_decimal(va_list args)
+{
+	unsigned int number, power = 1;
+	int n = va_arg(args, int), counter = 0;
+
+	number = n;
+	while (n / 10)
+	{
+		power = power * 10;
+		n /= 10;
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		counter++;
+		number = number * -1;
+	}
+	while (power >  0)
+	{
+		if (!(power == 1))
+		{
+			_putchar((number / power) + '0');
+			counter++;
+			number = number - (power * (number / power));
+		}
+		else
+		{
+			_putchar(number + '0');
+			counter++;
+		}
+		power /= 10;
+	}
+	return (counter);
+}
