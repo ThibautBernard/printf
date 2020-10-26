2a84edb11caab7a32037dc66034b3c276c447ec9
fourbet <ofourbet@yahoo.fr>
2020-10-26 11:36:06 +0000

    feat (print_unsigned.c, print_octal.c, print_hexa.c, print_hexaUpper.c)

--- holberton.h
+++ holberton.h
@@ -21,7 +21,13 @@ int _strlen(char *s);
 int print_char(va_list);
 int print_decimal(va_list);
 int print_string(va_list arg);
-
+int print_unsigned(va_list arg);
+int print_octal(va_list arg);
 int print_binary(va_list arg);
-
+int print_hexa(va_list arg);
+int print_hexaUpper(va_list arg);
+int print_rev_array(unsigned int max_size_array, char *array);
+int print_rev_array_hexa(unsigned int max_size_array, char *array);
+unsigned int _pow_recursion(unsigned int x, unsigned int y);
+char *fill_binary(unsigned int nb, unsigned int counter);
 #endif
